126d78f02d020b35fb63414f4aa6215cd7b38837
Tufar <73035932+Tufar1@users.noreply.github.com>
2024-08-23 16:42:17 +0200

    Update Math.hpp

--- ACExternal/Math.hpp
+++ ACExternal/Math.hpp
@@ -3,76 +3,88 @@
 #include <array>
 #include <corecrt_math.h>
 
-RECT rctAC;
-HWND hwndAC = FindWindow("SDL_app", "AssaultCube");
-
 struct Vector2
 {
-    float x;
-    float y;
+    float x{ 0 };
+    float y{ 0 };
+
+    friend Vector2 operator- (const Vector2& a, const Vector2& b) {
+        return Vector2{ a.x - b.x, a.y - b.y};
+    }
+    
+    friend Vector2 operator+ (const Vector2& a, const Vector2& b) {
+        return Vector2{ a.x + b.x, a.y + b.y};
+    }
+
+    friend Vector2 operator/(const Vector2& a, const float scalar){
+        return { a.x / scalar, a.y / scalar };
+    }
 };
 
 struct Vector3
 {
-    float x;
-    float y;
-    float z;
+    float x{ 0 };
+    float y{ 0 };
+    float z{ 0 };
 
     friend Vector3 operator- (const Vector3& a, const Vector3& b) {
         return Vector3{ a.x - b.x, a.y - b.y, a.z - b.z };
-    }
-
-    float Length() const {
-        return ((x * x) + (y * y) + (z * z));
     }
 };
 
 struct Vector4
 {
-    float x;
-    float y;
-    float z;
-    float w;
+    float x{ 0 };
+    float y{ 0 };
+    float z{ 0 };
+    float w{ 0 };
 };
 
 struct glMatrix
 {
-    std::array<float, 16> matrix;
+    std::array<float, 16> matrix{0};
 };
 
-bool WorldToScreen(Vector3 pos, Vector2& screenPos,glMatrix matrix)
-{
-    GetClientRect(hwndAC, &rctAC);
-    int windowWidth = rctAC.right;
-    int windowHeight = rctAC.bottom;
+class Math {
+private:
+    RECT client_rect{};
+    int windowWidth = 0;
+    int windowHeight = 0;
+public:
+    Math() = default;
 
-    //Matrix-vector Product, multiplying world(eye) coordinates by projection matrix = clipCoords
-    Vector4 clipCoords;
-    clipCoords.x = pos.x * matrix.matrix[0] + pos.y * matrix.matrix[4] + pos.z * matrix.matrix[8] + matrix.matrix[12];
-    clipCoords.y = pos.x * matrix.matrix[1] + pos.y * matrix.matrix[5] + pos.z * matrix.matrix[9] + matrix.matrix[13];
-    clipCoords.z = pos.x * matrix.matrix[2] + pos.y * matrix.matrix[6] + pos.z * matrix.matrix[10] + matrix.matrix[14];
-    clipCoords.w = pos.x * matrix.matrix[3] + pos.y * matrix.matrix[7] + pos.z * matrix.matrix[11] + matrix.matrix[15];
+    void init(){
+        wil::unique_hwnd hwnd{ FindWindow("SDL_app", "AssaultCube") };
+        GetClientRect(hwnd.get(), &client_rect);
+        windowWidth = client_rect.right;
+        windowHeight = client_rect.bottom;
+    }
 
-    if (clipCoords.w < 0.1f)
-        return false;
+    bool WorldToScreen(Vector3 pos, Vector2& screenPos, glMatrix matrix)
+    {
+        Vector4 clipCoords{ 0 };
+        clipCoords.x = pos.x * matrix.matrix[0] + pos.y * matrix.matrix[4] + pos.z * matrix.matrix[8] + matrix.matrix[12];
+        clipCoords.y = pos.x * matrix.matrix[1] + pos.y * matrix.matrix[5] + pos.z * matrix.matrix[9] + matrix.matrix[13];
+        clipCoords.z = pos.x * matrix.matrix[2] + pos.y * matrix.matrix[6] + pos.z * matrix.matrix[10] + matrix.matrix[14];
+        clipCoords.w = pos.x * matrix.matrix[3] + pos.y * matrix.matrix[7] + pos.z * matrix.matrix[11] + matrix.matrix[15];
 
-    //Perspective division, dividing by clip.W = Normalized Device Coordinates
-    Vector3 NDC;
-    NDC.x = clipCoords.x / clipCoords.w;
-    NDC.y = clipCoords.y / clipCoords.w;
-    NDC.z = clipCoords.z / clipCoords.w;
+        if (clipCoords.w < 0.1f)
+            return false;
 
-    //Transform to window coordinates
-    screenPos.x = (windowWidth / 2 * NDC.x) + (NDC.x + windowWidth / 2);
-    screenPos.y = -(windowHeight / 2 * NDC.y) + (NDC.y + windowHeight / 2);
-    return true;
-}
+        //Perspective division, dividing by clip.W = Normalized Device Coordinates
+        Vector3 NDC{ 0 };
+        NDC.x = clipCoords.x / clipCoords.w;
+        NDC.y = clipCoords.y / clipCoords.w;
+        NDC.z = clipCoords.z / clipCoords.w;
+
+        //Transform to window coordinates
+        screenPos.x = (windowWidth / 2 * NDC.x) + (NDC.x + windowWidth / 2);
+        screenPos.y = -(windowHeight / 2 * NDC.y) + (NDC.y + windowHeight / 2);
+        return true;
+    }
 
 
-double GetDistance(Vector2 enemy) {
-    GetClientRect(hwndAC, &rctAC);
-    int windowWidth = rctAC.right;
-    int windowHeight = rctAC.bottom;
-
-    return sqrt(pow((enemy.x - windowWidth / 2), 2) + pow((enemy.y - windowHeight / 2), 2));
-}
+    double GetDistance(Vector2 enemy) {
+        return std::sqrt(std::pow((enemy.x - windowWidth / 2), 2) + std::pow((enemy.y - windowHeight / 2), 2));
+    }
+};
